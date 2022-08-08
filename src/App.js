62d64ffd3936c83dc2b90902e298773c66ab9f67
Your Name <you@example.com>
2022-08-08 18:57:55 +0600

    navbar and banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div>
-    
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
